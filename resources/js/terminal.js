fb1722d77ce899539a02c3921b8a5c2648483baf
Nikolas Teuschl <niklas.teuschl@hotmail.com>
2019-10-16 20:27:43 +0200

    Fixed a resizing bug

--- resources/js/terminal.js
+++ resources/js/terminal.js
@@ -3,4 +3,8 @@ function getTerminalSrc() {
     let baseUrl = window.location.hostname;
     console.log(baseUrl);
     tm.setAttribute("src", "http://" + baseUrl + ":3636");
+
+    setTimeout( () => {
+        tm.style.height = '80vh';
+    }, 5000);
 }
